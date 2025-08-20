bf1fc63ddf87969530791f17a15688c96c7445b9
RobinJesba <jb.robinm@gmail.com>
2025-08-20 22:34:53 +0530

    refactor: remove unused HttpRequest interface from types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,3 @@
-import { Request, Response } from 'express';
-
-export interface HttpRequest {
-  (req: Request, res: Response): any;
-}
-
 export interface LogCorrelationConfig {
   projectId?: string;
   environment?: string;
